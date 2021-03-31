59bb749cc094db9e1fce4412704b3c25ff8d6048
oviebd <oviecse48@gmail.com>
2021-03-31 13:40:51 +0600

    Add Bullet System

---
+++ Assets/Scripts/Gun/BulletBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBase : DamagerBase
+{
+	private float _speed = 5.0f;
+	private Vector3 _direction = Vector3.right;
+
+	[SerializeField] private GameObject graphicsObj;
+
+
+	private Collider2D _collider2D;
+	private bool _canMove = true;
+
+	private void Start()
+	{
+		_collider2D = GetComponent<Collider2D>();
+	}
+
+	public void SetUp(float speed, Vector3 direction)
+	{
+		this._direction = direction;
+		this._speed = speed;
+		_canMove = true;
+	}
+
+	private void Update()
+	{
+		if (_canMove)
+			transform.position = transform.position + _direction * _speed * Time.deltaTime;
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{	
+		InActiveBullet();
+	}
+
+	private void InActiveBullet()
+	{
+		graphicsObj.SetActive(false);
+		_canMove = false;
+		_collider2D.enabled = false;
+
+		Invoke("DestroyBullet", .5f);
+	}
+
+	void DestroyBullet()
+	{
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private float bulletSpeed = 5.0f;
+	[SerializeField] private Vector3 bulletDirection = Vector3.right;
+
+
+	[SerializeField] private float bulletSpawnDelay = 1.0f;
+	[SerializeField] private GameObject gunPoint;
+
+	private bool _canSpawn = true;
+	private float _lastSpawnTime;
+
+
+	private void Update()
+	{
+		if (_canSpawn && (Time.time - _lastSpawnTime) >= bulletSpawnDelay )
+		{
+			SpawnBullet();
+		}
+	}
+
+
+	private void SpawnBullet()
+	{
+		GameObject bullet = Instantiate(bulletPrefab, gunPoint.transform.position, Quaternion.identity);
+		bullet.transform.SetParent(this.gameObject.transform, true);
+		bullet.transform.localScale = bulletPrefab.transform.localScale;
+		BulletBase bulletBase = bullet.GetComponent<BulletBase>();
+		bulletBase.SetUp(bulletSpeed, bulletDirection);
+		_lastSpawnTime = Time.time;
+	}
+
+
+
+
+}
